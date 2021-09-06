1f3f6ce7c57b57c55e190dda1592602a803a9c5b
Maksim <boltoff1999@yandex.ru>
2021-09-06 23:57:09 +0300

    Add swagger documentation.

--- project/urls.py
+++ project/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from .yasg import urlpatterns as yasg_urls
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
     path("api-auth/", include("rest_framework.urls")),
 ]
+
+urlpatterns += yasg_urls
